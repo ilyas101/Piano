56a9daff29f7cf1bf7e0ea1933b512c10e1525c1
ilyas <ikarimov@kazan.amfitel.ru>
2019-12-20 15:51:56 +0300

    refactoring tests

--- src/test/java/ru/karimov/piano/MainControllerTest.java
+++ src/test/java/ru/karimov/piano/MainControllerTest.java
@@ -1,14 +1,5 @@
 package ru.karimov.piano;
 
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,21 +8,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 /**
- * Created by 777 on 08.12.2018.
+ * Created by ikarimov on 20.12.2019.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class PianoApplicationTest {
+public class MainControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void testBlankPage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Search")));
+    public void blankPageStatusIsOK() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
@@ -39,12 +40,8 @@ public class PianoApplicationTest {
         this.mockMvc.perform(post("/")
                 .param("query", "java"))
                 .andExpect(status().isOk())
+                .andExpect(model().attributeExists("items"))
                 .andExpect(content().string(containsString("Title")))
                 .andExpect(content().string(containsString("Next")));
-
-        this.mockMvc.perform(get("/2"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Previous")));
     }
-
 }
